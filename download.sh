05059f490337f57bd340993dfe8848e66b209f97
Olaf Iseger <olaf.iseger@geodan.nl>
2017-03-21 12:51:26 +0100

    Add parameters for URL and data-directory

--- download.sh
+++ download.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+# Accept 2 parameters:
+# - url of sheet (no default)
+# - location to download to (default: ./)
+
+URL=${1}
+DATA_DIRECTORY=${2}
+
+if [[ ${URL} == "" ]]
+then
+  echo "Usage download.sh <URL> <data-directory>"
+  exit
+fi
+
 MONTH=`date +%m`
 YEAR=`date +%Y`
-FILE="data/${YEAR}_${MONTH}.tsv"
 
-curl "https://docs.google.com/spreadsheets/d/1HvsbibsBTBvZXDaJZWYyHwTxuA7a4eEbgcIKaPhuoxs/export?gid=0&format=tsv" -o "$FILE"
+if [[ ${DATA_DIRECTORY} == "" ]]
+then
+  FILE="data/${YEAR}_${MONTH}.tsv"
+  mkdir -p data
+else
+  FILE="${DATA_DIRECTORY}/data/${YEAR}_${MONTH}.tsv"
+  mkdir -p "${DATA_DIRECTORY}/data/"
+fi
+
+
+curl "${URL}" -o "${FILE}"
 
 #remove first row
-tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
+tail -n +2 "${FILE}" > "$FILE.tmp" && mv "$FILE.tmp" "${FILE}"
